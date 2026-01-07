7767d08183b9c1a6bcb0de85717e403e9f8f31df
toufikunsure-4529 <devtoufik@gmail.com>
2026-01-07 15:47:59 +0530

    `Added import of Script from "next/script" in app/layout.js`

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ClientProviders from "./ClientProviders";
 import UserAgentFix from "./UserAgentFix";
-
+import Script from "next/script";
 /* Optional fonts (kept for future use) */
 const geistSans = Geist({
   variable: "--font-geist-sans",
