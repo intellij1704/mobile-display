9eb8215bcc38a2eaa83a5ac4968790fbc87d53bb
toufikunsure-4529 <devtoufik@gmail.com>
2026-01-07 15:43:52 +0530

    `Update Tawk.to Live Chat script in RootLayout`

--- app/layout.js
+++ app/layout.js
@@ -85,11 +85,24 @@ export default function RootLayout({ children }) {
           <UserAgentFix />
           {children}
         </ClientProviders>
-           {/* Tawk.to Live Chat */}
+     {/* Tawk.to Live Chat */}
         <Script
           id="tawk-to"
           strategy="afterInteractive"
-          src="https://embed.tawk.to/695e2dc221ac6f19798bddba/1jebu5gcd"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),
+                    s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/695e2dc221ac6f19798bddba/1jebu5gcd';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
         />
       </body>
     </html>
